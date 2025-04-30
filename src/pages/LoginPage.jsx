ea19e78b988cffdada347ea4f434a8897d6b5f38
Niranjan VM <niranjanvm739@gmail.com>
2025-05-01 02:18:34 +0530

    Update login API URL to deployed backend

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -12,7 +12,7 @@ function LoginPage() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:5000/api/auth/login', { username, password });
+            const response = await axios.post('https://movie-backend-djdp.onrender.com/api/auth/login', { username, password });
             localStorage.setItem('token', response.data.token);
             navigate('/');
         } catch (err) {
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -12,7 +12,7 @@ function RegisterPage() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            await axios.post('http://localhost:5000/api/auth/register', { username, password });
+            await axios.post('http:https://movie-backend-djdp.onrender.com/api/auth/register', { username, password });
             navigate('/login');
         } catch (err) {
             setError('Error creating account');
